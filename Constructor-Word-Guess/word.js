fb94371c9a8eea1ba944b1485677c88831efbac5
Jay <jasonnguyen@Jays-MacBook-Pro.local>
2019-05-28 17:55:18 -0400

    word/letter.js

---
+++ Constructor-Word-Guess/word.js
@@ -0,0 +1,26 @@
+var letter = require("./letter.js");
+
+function word(answer) {
+    this.objArray = [];
+
+    for(var i=0; i< answer.length; i++){
+        var letter = new letter(answer[i]);
+        this.objArray.push(letter);
+    }
+
+    this.log = function(){
+        answerLog = "";
+        for(var i = 0; i<this.objArray.length; i++){
+            answerLog += this.objArray[i] + " ";
+        }
+        
+    };
+
+    this.userGuess = function(input){
+        for (var i=0; i<this.objArray.length; i++){
+            this.objArray[i].guess(input);
+        }
+    };
+}
+
+module.exports = word;
